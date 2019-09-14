1dfa8a229548d6c98a3c58b60bb50f1e65c7e035
DrStupit <kerevreddy@hotmail.com>
2019-09-14 06:35:48 +0200

    untapped-culture initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListingComponent } from './listing/listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listing', component: ListingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatListModule, MatButtonModule, MatAutocompleteModule, MatInputModule} from '@angular/material';
+import {MatSidenavModule, MatExpansionModule, MatToolbarModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ListingComponent } from './listing/listing.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ListingComponent,
+      ToolbarComponent,
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatAutocompleteModule,
+      MatInputModule,
+      MatSidenavModule,
+      MatExpansionModule,
+      MatCardModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatListModule,
+      MatButtonModule,
+      FormsModule,
+      ReactiveFormsModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
